1985ececaf5d001a9bd945b9e3dbce6e6f4a4931
Martin Milata <mmilata@redhat.com>
2011-09-22 17:06:05 +0200

    a-a-generate-canonical-backtrace: program skeleton

---
+++ src/plugins/abrt-action-generate-canonical-backtrace.c
@@ -0,0 +1,68 @@
+/*
+    Copyright (C) 2011  ABRT team
+    Copyright (C) 2011  RedHat Inc
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+#include "abrtlib.h"
+
+struct backtrace_entry {
+    unsigned long address;
+    char *build_id;
+    unsigned build_id_offset;
+    char *symbol;
+    char *modname;
+    char *fingerprint;
+    /* fde_entry ptr? */
+    /* linked list next ptr? */
+    /* pointer to function disassembly? */
+};
+
+struct fde_entry {
+    unsigned long start;
+    unsigned length;
+};
+
+int main(int argc, char **argv)
+{
+    abrt_init(argv);
+
+    const char *dump_dir_name = ".";
+
+    /* Can't keep these strings/structs static: _() doesn't support that */
+    const char *program_usage_string = _(
+        "\1 [-v] -d DIR\n"
+        "\n"
+        "Creates canonical backtrace from core dump and corresponding binary"
+    );
+    enum {
+        OPT_v = 1 << 0,
+        OPT_d = 1 << 1,
+    };
+    /* Keep enum above and order of options below in sync! */
+    struct options program_options[] = {
+        OPT__VERBOSE(&g_verbose),
+        OPT_STRING('d', NULL, &dump_dir_name, "DIR", _("Dump directory")),
+        OPT_END()
+    };
+    /*unsigned opts =*/ parse_opts(argc, argv, program_options, program_usage_string);
+
+    export_abrt_envvars(0);
+
+    /* gdb - get addresses */
+    /* eu-unstrip - build ids and library paths*/
+
+    return 0;
+}
