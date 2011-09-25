b9fe16d535db10b20dfe771324c5407dd7a50d6c
Martin Milata <mmilata@redhat.com>
2011-09-25 14:47:39 +0200

    a-a-g-c-b: Run gdb and read its output

--- src/plugins/abrt-action-generate-canonical-backtrace.c
+++ src/plugins/abrt-action-generate-canonical-backtrace.c
@@ -18,6 +18,9 @@
 */
 #include "abrtlib.h"
 
+/* 60 seconds was too limiting on slow machines */
+static int exec_timeout_sec = 240;
+
 struct backtrace_entry {
     unsigned long address;
     char *build_id;
@@ -34,6 +37,89 @@ struct fde_entry {
     unsigned long start;
     unsigned length;
 };
+
+/* copypasted from abrt-action-generate-backtrace */
+static char *get_gdb_output(const char *dump_dir_name)
+{
+    struct dump_dir *dd = dd_opendir(dump_dir_name, /*flags:*/ 0);
+    if (!dd)
+        return NULL;
+
+    char *uid_str = dd_load_text_ext(dd, FILENAME_UID, DD_FAIL_QUIETLY_ENOENT | DD_LOAD_TEXT_RETURN_NULL_ON_FAILURE);
+    uid_t uid = -1L;
+    if (uid_str)
+    {
+        uid = xatoi_positive(uid_str);
+        free(uid_str);
+        if (uid == geteuid())
+        {
+            uid = -1L; /* no need to setuid/gid if we are already under right uid */
+        }
+    }
+    char *executable = dd_load_text(dd, FILENAME_EXECUTABLE);
+    dd_close(dd);
+
+    char *args[11];
+    args[0] = (char*)"gdb";
+    args[1] = (char*)"-batch";
+
+    /* NOTE: Do we need this or is it an artifact from using nonsystem debug dirs? */
+    args[2] = (char*)"-ex";
+    args[3] = (char*)"set debug-file-directory /usr/lib/debug";
+
+    /* "file BINARY_FILE" is needed, without it gdb cannot properly
+     * unwind the stack. Currently the unwind information is located
+     * in .eh_frame which is stored only in binary, not in coredump
+     * or debuginfo.
+     *
+     * Fedora GDB does not strictly need it, it will find the binary
+     * by its build-id.  But for binaries either without build-id
+     * (= built on non-Fedora GCC) or which do not have
+     * their debuginfo rpm installed gdb would not find BINARY_FILE
+     * so it is still makes sense to supply "file BINARY_FILE".
+     *
+     * Unfortunately, "file BINARY_FILE" doesn't work well if BINARY_FILE
+     * was deleted (as often happens during system updates):
+     * gdb uses specified BINARY_FILE
+     * even if it is completely unrelated to the coredump.
+     * See https://bugzilla.redhat.com/show_bug.cgi?id=525721
+     *
+     * TODO: check mtimes on COREFILE and BINARY_FILE and not supply
+     * BINARY_FILE if it is newer (to at least avoid gdb complaining).
+     */
+    args[4] = (char*)"-ex";
+    args[5] = xasprintf("file %s", executable);
+    free(executable);
+
+    args[6] = (char*)"-ex";
+    args[7] = xasprintf("core-file %s/"FILENAME_COREDUMP, dump_dir_name);
+
+    args[8] = (char*)"-ex";
+    /*args[9] = ... see below */
+    args[10] = NULL;
+
+    /* Get the backtrace, but try to cap its size */
+    /* Limit bt depth. With no limit, gdb sometimes OOMs the machine */
+    unsigned bt_depth = 2048;
+    char *bt = NULL;
+    while (1)
+    {
+        args[9] = xasprintf("backtrace %u", bt_depth);
+        bt = exec_vp(args, uid, /*redirect_stderr:*/ 1, exec_timeout_sec, NULL);
+        free(args[9]);
+        if ((bt && strnlen(bt, 256*1024) < 256*1024) || bt_depth <= 32)
+        {
+            break;
+        }
+
+        free(bt);
+        bt_depth /= 2;
+    }
+
+    free(args[5]);
+    free(args[7]);
+    return bt;
+}
 
 int main(int argc, char **argv)
 {
@@ -61,7 +147,15 @@ int main(int argc, char **argv)
 
     export_abrt_envvars(0);
 
-    /* gdb - get addresses */
+
+    VERB1 log("Querying gdb for backtrace");
+    char *gdb_out = get_gdb_output(dump_dir_name);
+    if (gdb_out == NULL)
+        return 1;
+
+    printf("%s\n", gdb_out);
+
+    /* parse addresses and eventual symbols from the output*/
     /* eu-unstrip - build ids and library paths*/
 
     return 0;
