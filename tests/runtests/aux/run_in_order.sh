b8b62b068de9d42f6d482c9247658c5bba8c6c82
Richard Marko <rmarko@redhat.com>
2011-11-11 15:18:31 +0100

    Save only post crashes

--- tests/runtests/aux/run_in_order.sh
+++ tests/runtests/aux/run_in_order.sh
@@ -12,21 +12,12 @@ for test_dir in $testlist; do
 
     mkdir -p $outdir
 
-    # save pre crashes
-    n_prior=$( find /var/spool/abrt/* -type d | wc -l )
-    if [ $n_prior -gt 0 ]; then
-        mkdir "$outdir/pre_crashes"
-        for dir in $( find /var/spool/abrt/* -type d ); do
-            mv "$dir" "$outdir/pre_crashes/"
-        done
-    fi
-
     syslog $short_testname
     $RUNNER_SCRIPT $test &> $logfile
 
     # save post crashes
-    n_prior=$( find /var/spool/abrt/* -type d | wc -l )
-    if [ $n_prior -gt 0 ]; then
+    n_post=$( find /var/spool/abrt/* -type d | wc -l )
+    if [ $n_post -gt 0 ]; then
         mkdir "$outdir/post_crashes"
         for dir in $( find /var/spool/abrt/* -type d ); do
             mv "$dir" "$outdir/post_crashes/"
@@ -37,7 +28,6 @@ for test_dir in $testlist; do
     start_line=$(grep -n -i 'Test protocol' $logfile | awk -F: '{print $1}')
     end_line=$(grep -n -i 'TEST END MARK' $logfile | awk -F: '{print $1}')
 
-    #echo "|$start_line|"
     if [ $start_line -gt 0 ]; then
         start_line=$[ $start_line - 1 ]
     fi
