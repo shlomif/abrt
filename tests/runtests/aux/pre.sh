a5617b372cff929abe9139a7be6f00fee031fb3d
Richard Marko <rmarko@redhat.com>
2011-11-12 22:03:25 +0100

    Collect dmesg in pre

--- tests/runtests/aux/pre.sh
+++ tests/runtests/aux/pre.sh
@@ -2,5 +2,6 @@
 
 export > $OUTPUT_ROOT/pre/envs.log
 cp /var/log/messages $OUTPUT_ROOT/pre/messages
+dmesg -c > $OUTPUT_ROOT/pre/dmesg
 
 yum install -y beakerlib dejagnu btparser-devel time createrepo mock
