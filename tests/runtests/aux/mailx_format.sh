3097331649416609909f290c504bd8fafcd8eab2
Richard Marko <rmarko@redhat.com>
2011-11-13 13:02:08 +0100

    Mailx formatting

--- tests/runtests/aux/mailx_format.sh
+++ tests/runtests/aux/mailx_format.sh
@@ -1,3 +1,23 @@
-touch $OUTPUT_ROOT/mail.summary
+
+pushd $OUTPUT_ROOT
+
+touch 'mail.summary'
+if grep -q 'FAIL' 'results'; then
+    export RESULT='FAIL'
+    echo 'Failed tests:' >> 'mail.summary'
+    for ft in $( find . -type f -name 'failed' ); do
+        dir=$( dirname $ft )
+        ts=$( basename $dir )
+
+        echo " - $ts:" >> 'mail.summary'
+        grep -i FAIL $dir >> 'mail.summary'
+        echo "" >> 'mail.summary'
+    done
+else
+    echo 'All tests passed' >> 'mail.summary'
+    export RESULT='PASS'
+fi
+
+popd
 
 tar czvf $OUTPUT_ROOT.tar.gz $OUTPUT_ROOT
