a4fcde3084c3686b3a2c67adfddbde0524695bba
Petr Kubat <pkubat@redhat.com>
2013-09-09 16:59:31 +0200

    add whitelist of env variables to debuginfo install wrapper - #692

--- src/plugins/abrt-action-install-debuginfo-to-abrt-cache.c
+++ src/plugins/abrt-action-install-debuginfo-to-abrt-cache.c
@@ -61,7 +61,7 @@ int main(int argc, char **argv)
     if (g != getgid())
         IGNORE_RESULT(setregid(g, g));
     uid_t u = geteuid();
-    if (u != getuid())
+    if (1)
     {
         IGNORE_RESULT(setreuid(u, u));
         /* We are suid'ed! */
@@ -70,7 +70,35 @@ int main(int argc, char **argv)
 // We forgot to sanitize PYTHONPATH. And who knows what else we forgot
 // (especially considering *future* new variables of this kind).
 // We switched to clearing entire environment instead:
+
+        // However since we communicate through environment variables
+        // we have to keep a whitelist of variables to keep.
+        static const char *whitelist[] = {
+            "REPORT_CLIENT_SLAVE" //  Check if the app is being run as a slave
+        };
+        int wlsize = sizeof(whitelist)/sizeof(char*);
+        char *setlist[wlsize];
+        char *p = NULL;
+        for (int i = 0; i < wlsize; i++) {
+            if ((p = getenv(whitelist[i])) != NULL) {
+                setlist[i] = malloc(strlen(p)*sizeof(char*));
+                setlist[i] = strncpy(setlist[i], p, strlen(p));
+            }
+            else
+                setlist[i] = NULL;
+        }
+        // Now we can clear the environment
         clearenv();
+        // And once again set whitelisted variables
+        for (int i = 0; i < wlsize; i++) {
+            if (setlist[i] != NULL) {
+                int slsize = sizeof(setlist)/sizeof(char*);
+                char buffer[slsize+wlsize+1];
+                sprintf(buffer, "%s=%s", whitelist[i], setlist[i]);
+                putenv(buffer);
+                free(setlist[i]);
+            }
+        }
 #else
         /* Clear dangerous stuff from env */
         static const char forbid[] =
